c20b9a698a9f13a7a56a1156a1ec8ac5a206e356
1582130940 <19632674+1582130940@users.noreply.github.com>
2025-10-25 06:49:32 +0800

    Optimizing performance & size
    Remove
    -static-libstdc++
    
    Add
    -O3
    -Dshader-cache=true
    -Dshader-cache-default=true
    -Dvulkan-beta=true

--- build-turnip.sh
+++ build-turnip.sh
@@ -111,8 +111,8 @@ echo "Creating Meson cross file..." $'\n'
 cat <<EOF >"android-aarch64.txt"
 [binaries]
 ar = '$ndk_bin/llvm-ar'
-c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
-cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '--start-no-unused-arguments', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '--end-no-unused-arguments', '-Wno-error=c++11-narrowing', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
+c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-O3', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
+cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '-O3', '--start-no-unused-arguments', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--end-no-unused-arguments', '-Wno-error=c++11-narrowing', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
 c_ld = '$ndk_bin/ld.lld'
 cpp_ld = '$ndk_bin/ld.lld'
 strip = '$ndk_bin/aarch64-linux-android-strip'
@@ -148,7 +148,10 @@ CC=clang CXX=clang++ meson setup build-android-aarch64 \
     -Dplatform-sdk-version="$sdkver" \
     -Dandroid-stub=true \
     -Dgallium-drivers= \
+    -Dshader-cache=true \
+    -Dshader-cache-default=true \
     -Dvulkan-drivers=freedreno \
+    -Dvulkan-beta=true \
     -Dfreedreno-kmds=kgsl \
     -Db_lto=true \
     -Degl=disabled \
