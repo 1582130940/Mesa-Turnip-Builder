b0885cd8ad477af988ec4644c7c0b303c3c8483e
1582130940 <19632674+1582130940@users.noreply.github.com>
2025-10-25 07:37:46 +0800

    Sync mesa main create-android-cross-file.sh/android.rst

--- build-turnip.sh
+++ build-turnip.sh
@@ -111,12 +111,11 @@ echo "Creating Meson cross file..." $'\n'
 cat <<EOF >"android-aarch64.txt"
 [binaries]
 ar = '$ndk_bin/llvm-ar'
-c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-O3', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
-cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '-O3', '-fno-rtti', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '--start-no-unused-arguments', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--end-no-unused-arguments', '-Wno-error=c++11-narrowing', '-Wno-deprecated-declarations', '-Wno-gnu-alignof-expression']
+c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-O3', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables']
+cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '-O3', '-fno-rtti', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '--end-no-unused-arguments']
 c_ld = '$ndk_bin/ld.lld'
 cpp_ld = '$ndk_bin/ld.lld'
 strip = '$ndk_bin/aarch64-linux-android-strip'
-pkg-config = ['env', 'PKG_CONFIG_LIBDIR=NDKDIR/pkg-config', '/usr/bin/pkg-config']
 
 [host_machine]
 system = 'android'
@@ -147,6 +146,7 @@ CC=clang CXX=clang++ meson setup build-android-aarch64 \
     -Dplatforms=android \
     -Dplatform-sdk-version="$sdkver" \
     -Dandroid-stub=true \
+    -Dandroid-libbacktrace=disabled \
     -Dgallium-drivers= \
     -Dshader-cache=enabled \
     -Dshader-cache-default=true \
