8d43391cd0f13d8111e32960f5cc13ca386e6584
1582130940 <19632674+1582130940@users.noreply.github.com>
2026-02-19 12:46:32 +0800

    Experimental optimization
    -ffast-math
    -Dandroid-strict=false
    -Dvideo-codecs=all

--- build-turnip.sh
+++ build-turnip.sh
@@ -111,8 +111,8 @@ echo "Creating Meson cross file..." $'\n'
 cat <<EOF >"android-aarch64.txt"
 [binaries]
 ar = '$ndk_bin/llvm-ar'
-c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-O3', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables']
-cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '-O3', '-fno-rtti', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '--end-no-unused-arguments']
+c = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang', '-O3', '-ffast-math', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables']
+cpp = ['ccache', '$ndk_bin/aarch64-linux-android$sdkver-clang++', '-O3', '-ffast-math', '-fno-rtti', '-DVK_USE_PLATFORM_ANDROID_KHR', '-fPIC', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '--end-no-unused-arguments']
 c_ld = '$ndk_bin/ld.lld'
 cpp_ld = '$ndk_bin/ld.lld'
 strip = '$ndk_bin/aarch64-linux-android-strip'
@@ -147,6 +147,7 @@ CC=clang CXX=clang++ meson setup build-android-aarch64 \
     -Dplatform-sdk-version="$sdkver" \
     -Dandroid-stub=true \
     -Dandroid-libbacktrace=disabled \
+    -Dandroid-strict=false \
     -Dgallium-drivers= \
     -Dshader-cache=enabled \
     -Dshader-cache-default=true \
@@ -158,6 +159,7 @@ CC=clang CXX=clang++ meson setup build-android-aarch64 \
     -Dcpp_rtti=false \
     -Dallow-fallback-for=libdrm \
     -Degl=disabled \
+    -Dvideo-codecs=all \
     -Dstrip=true &> $workdir/meson_log
 
 # Compile build files using Ninja
