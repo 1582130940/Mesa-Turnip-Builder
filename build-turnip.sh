6bf4e3d5b87b79fd848739777cbd29fb2349ccbb
1582130940 <19632674+1582130940@users.noreply.github.com>
2026-02-19 13:40:13 +0800

    Rename Turnip Adreno to Adreno

--- build-turnip.sh
+++ build-turnip.sh
@@ -86,6 +86,9 @@ cd $mesadir
 echo "Applying patch: Enable all Vulkan extensions for Android..." $'\n'
 patch -p1 < ../../patches/Enable-all-Vulkan-extensions-for-Android.patch
 
+echo "Applying OEM naming patch..."
+sed -i 's/"Turnip Adreno/"Adreno/g' "$workdir/$mesadir/src/freedreno/vulkan/tu_device.cc"
+
 # Set NDK Clang bin directory
 ndk_bin="$workdir/$ndkdir/toolchains/llvm/prebuilt/linux-x86_64/bin"
 
